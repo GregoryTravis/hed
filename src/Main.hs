3d6f75032d33ac4a72f48516f031136ff5425d7e
Gregory Travis <greg.m.travis@gmail.com>
2019-03-01 08:53:53 -0500

    bring in ansi-terminal

--- src/Main.hs
+++ src/Main.hs
@@ -1,1 +1,11 @@
-main = putStrLn "hi"
+module Main where
+
+import System.Console.ANSI
+
+-- Set colors and write some text in those colors.
+main = do
+  setSGR [SetColor Foreground Vivid Red]
+  setSGR [SetColor Background Vivid Blue]
+  putStrLn "Red-On-Blue"
+  setSGR [Reset]  -- Reset to default colour scheme
+  putStrLn "Default colors."
