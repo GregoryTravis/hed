dd7672090874360e4048946011d4e587a0619e86
Gregory Travis <greg.m.travis@gmail.com>
2019-04-21 13:13:55 -0400

    more monoidy

--- src/Thing.hs
+++ src/Thing.hs
@@ -7,7 +7,7 @@ data Thing = Thing Char
   deriving (Eq, Show)
 
 renderThing :: Thing -> (Int, Int) -> String
-renderThing (Thing c) (w, h) = mconcat $ [blank] ++ lines ++ [blank]
+renderThing (Thing c) (w, h) = blank <> mconcat lines <> blank
   where blank = take w (repeat ' ')
         lines = take (h-2) (repeat line)
-        line = " " ++ (take (w-2) (repeat c)) ++ " "
+        line = " " <> (take (w-2) (repeat c)) <> " "
