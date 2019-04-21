e6a20b8b7a7f66e3ff593aab71a1acb57d6f80f5
Gregory Travis <greg.m.travis@gmail.com>
2019-04-21 15:40:54 -0400

    Buf takes name instea of buffer.

--- src/Display.hs
+++ src/Display.hs
@@ -16,21 +16,16 @@ import Util
 redisplay :: ESAction ()
 redisplay = do
   s <- get
-  let t = buffers s M.! currentBuffer s
-  let other = buffers s M.! "hey"
   io $ do
     clearScreen
     setCursorPosition 0 0
     case screenDim s of Nothing -> msp "Can't determine screen dimensions"
                         Just dim -> withStdoutBuffering (BlockBuffering Nothing) $ do 
                                       setCursorPosition 0 0
-                                      putStr $ mconcat $ checkSize (renderLayout (VStack (Buf t) (Buf other)) dim) dim
-                                      --putStr $ mconcat $ checkSize (renderBuffer t dim) dim
-                                      --putStrLn $ show $ length $ checkSize (renderBuffer t dim) dim
+                                      putStr $ mconcat $ checkSize (renderLayout s (VStack (Buf (currentBuffer s)) (Buf "hey")) dim) dim
                                       --msp s
                                       setCursorPosition 0 0
                                       hFlush stdout
-  --where checkSize s (w, h) = assertM "dim" (length s == w*h) s
   where checkSize :: [String] -> (Int, Int) -> [String]
         checkSize s (w, h) = assertM "dim" ok s
           where ok = (length s == h) && (all (w==) (map length s))
--- src/Layout.hs
+++ src/Layout.hs
@@ -3,20 +3,23 @@ module Layout
 , renderLayout
 ) where
 
+import qualified Data.Map as M
+
 import Buffer
+import State
 
-data Layout = Buf Buffer | HStack Layout Layout | VStack Layout Layout
+data Layout = Buf String | HStack Layout Layout | VStack Layout Layout
 
-renderLayout :: Layout -> (Int, Int) -> [String]
-renderLayout (Buf buffer) dim = renderBuffer buffer dim
-renderLayout (VStack top bottom) (w, h) = vConcat w topR bottomR
-  where topR = renderLayout top (w, topH)
-        bottomR = renderLayout bottom (w, bottomH)
+renderLayout :: EditorState -> Layout -> (Int, Int) -> [String]
+renderLayout es (Buf bufferName) dim = renderBuffer (buffers es M.! bufferName) dim
+renderLayout es (VStack top bottom) (w, h) = vConcat w topR bottomR
+  where topR = renderLayout es top (w, topH)
+        bottomR = renderLayout es bottom (w, bottomH)
         topH = (h-1) `div` 2
         bottomH = h - topH - 1
-renderLayout (HStack left right) (w, h) = hConcat leftR rightR
-  where leftR = renderLayout left (leftW, h)
-        rightR = renderLayout right (rightW, h)
+renderLayout es (HStack left right) (w, h) = hConcat leftR rightR
+  where leftR = renderLayout es left (leftW, h)
+        rightR = renderLayout es right (rightW, h)
         leftW = (w-1) `div` 2
         rightW = w - leftW - 1
 
