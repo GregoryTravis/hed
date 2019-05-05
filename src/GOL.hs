4c8d8b902166c078bae167da1ce29bf8164b7360
Gregory Travis <greg.m.travis@gmail.com>
2019-05-05 15:12:00 -0400

    mapping invert over it

--- src/GOL.hs
+++ src/GOL.hs
@@ -1,5 +1,26 @@
 module GOL
 ( gol ) where
 
+import Data.List (intercalate)
+import Data.List.Split
+import qualified Data.Vector as V
+
+type Grid = V.Vector (V.Vector Bool)
+
 gol :: String -> String
-gol = reverse
+gol = gridToString . gol' . stringToGrid
+gol' :: Grid -> Grid
+gol' g = V.map (V.map not) g
+
+charToBool '.' = False
+charToBool 'o' = True
+boolToChar False = '.'
+boolToChar True = 'o'
+
+stringToGrid :: String -> Grid
+stringToGrid s = V.fromList $ map V.fromList (map (map charToBool) lines)
+  where lines = splitOn "\n" s
+
+gridToString :: Grid -> String
+gridToString g = join $ map (map boolToChar) $ map V.toList $ V.toList g
+  where join = intercalate "\n"
