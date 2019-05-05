e1b39656cee1458857f76518f8563bc08b104fef
Gregory Travis <greg.m.travis@gmail.com>
2019-05-05 12:57:38 -0400

    fixed

--- src/Buffer.hs
+++ src/Buffer.hs
@@ -10,14 +10,14 @@ import Types
 import Util
 
 renderBuffer :: Buffer -> (Int, Int) -> (Int, Int) -> [String]
-renderBuffer buf (x, y) (w, h) = assertM "offset" ok hPaddedLines
+renderBuffer buf (ox, oy) (w, h) = assertM "offset" ok hPaddedLines
   where contents = bufferContents buf
-        lines = map (drop x) $ drop y $ take h $ splitOn "\n" contents
+        lines = map (drop ox) $ take h $ drop oy $ splitOn "\n" contents
         pad line = (take w line) ++ take (w - (length line)) (repeat ' ')
         paddedLines = map pad lines
         hPaddedLines = paddedLines ++ (take (h - length paddedLines) $ repeat blankLine)
         blankLine = take w (repeat ' ')
-        ok = x >=0 && y >= 0
+        ok = ox >=0 && oy >= 0
 
 appendToBuffer :: Buffer -> String -> Buffer
 appendToBuffer buf s = buf { bufferContents = bufferContents buf ++ s }
